1cfb52787903906b7cb77d3aeb5accc800ca7e91
Roberto Vasquez Angel <robotex@robotex.de>
2011-07-24 05:19:37 +0200

    Fixed migration

--- lib/generators/ecm/toplists/install/templates/create_toplists.rb
+++ lib/generators/ecm/toplists/install/templates/create_toplists.rb
@@ -1,7 +1,7 @@
 class <%= migration_name %> < ActiveRecord::Migration
   def self.up
     create_table :<%= table_name %> do |t|
-      t.string :list_order
+      t.integer :list_order
       t.string :name
       t.text :description
       t.string :link
